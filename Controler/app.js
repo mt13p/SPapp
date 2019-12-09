d8820b22fd3797b67631d6a72efbdf79b2c98795
mt13p <kozakartem@gmail.com>
2019-12-09 22:12:04 +0200

    Add button Jump to Top

--- Controler/app.js
+++ Controler/app.js
@@ -43,6 +43,45 @@ app.controller('MainMenu', function($scope, $mdSidenav, $location, $anchorScroll
         $mdSidenav(componentId).toggle();
       };
     }
+ 
+// Set a variable for our button element.
+const scrollToTopButton = document.getElementById('js-top');
+
+// Let's set up a function that shows our scroll-to-top button if we scroll beyond the height of the initial window.
+const scrollFunc = () => {
+  // Get the current scroll value
+  let y = window.scrollY;
+  
+  // If the scroll value is greater than the window height, let's add a class to the scroll-to-top button to show it!
+  if (y > 0) {
+    scrollToTopButton.className = "top-link show";
+  } else {
+    scrollToTopButton.className = "top-link hide";
+  }
+};
+
+window.addEventListener("scroll", scrollFunc);
+
+const scrollToTop = () => {
+  // Let's set a variable for the number of pixels we are from the top of the document.
+  const c = document.documentElement.scrollTop || document.body.scrollTop;
+  
+  // If that number is greater than 0, we'll scroll back to 0, or the top of the document.
+  // We'll also animate that scroll with requestAnimationFrame:
+  // https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame
+  if (c > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    // ScrollTo takes an x and a y coordinate.
+    // Increase the '10' value to get a smoother/slower scroll!
+    window.scrollTo(0, c - c / 10);
+  }
+};
+
+// When the button is clicked, run our ScrolltoTop function above!
+scrollToTopButton.onclick = function(e) {
+  e.preventDefault();
+  scrollToTop();
+}
  });
 
 app.controller('DemoCtrl', function($scope, $mdDialog, $timeout, $rootScope, $mdUtil) {
@@ -118,33 +157,7 @@ app.controller('DemoCtrl', function($scope, $mdDialog, $timeout, $rootScope, $md
       };
 });
  
-/* /** Directive which applies a specified class to the element when being scrolled */
- app.directive('docsScrollClass', function() {
-    return {
-     restrict: 'A',
-     link: function(scope, element, attr) {
 
-       var scrollParent = element.parent();
-       var isScrolling = false;
-
-       // Initial update of the state.
-       updateState();
-
-       // Register a scroll listener, which updates the state.
-       scrollParent.on('scroll', updateState);
-
-       function updateState() {
-         var newState = scrollParent[0].scrollTop !== 0;
-
-        if (newState !== isScrolling) {
-           element.toggleClass(attr.docsScrollClass, newState);
-         }
-
-         isScrolling = newState;
-       }
-     }  
-   }
- });
 
 app.controller('HomeController', function($scope, $rootScope) {
   $scope.message = 'Головна сторінка';
